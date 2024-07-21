f01253b9a9f83b8677af343dbddac0537d70f5e6
Ghulam89 <gm6681328@gmail.com>
2024-07-21 10:42:52 -0700

    update

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -4,11 +4,19 @@ import { FaAngleDown, FaArrowRight } from "react-icons/fa6";
 import Header from "../header";
 import Counter from "../Counter";
 import VideoPlayer from "../videoPlayer";
+import { FaAngleUp } from "react-icons/fa";
 const Hero = () => {
   const [selectedCurrency, setSelectedCurrency] = useState("USDT");
 
   const handleSelect = (currency) => {
     setSelectedCurrency(currency);
+  };
+
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
   };
 
   const getBorderColor = (currency) => {
@@ -211,148 +219,121 @@ GET VIDEO <FaArrowRight color="#456DA7" />
 
 
 
-                    <div className=" tw-pt-6 tw-flex tw-justify-center ">
-                      <p className=" tw-flex  tw-gap-3 tw-items-center tw-text-md  tw-font-poppins tw-font-semibold">REFERRAL DETAILS <FaAngleDown/> </p>
-                    </div>
+                    <div className="tw-pt-6 tw-flex tw-justify-center">
+      <p
+        className="tw-flex tw-gap-3 tw-items-center tw-text-md tw-font-poppins tw-font-semibold cursor-pointer"
+        onClick={handleToggle}
+      >
+        REFERRAL DETAILS {isExpanded ? <FaAngleUp/> : <FaAngleDown />}
+      </p>
+      
+    </div>
 
 
-
-                    <table className="tw-min-w-full tw-mb-0">
-                    <thead className="   tw-border-t tw-border-b tw-border-[#456DA7] tw-bg-primary">
-                      <tr className=" tw-rounded-lg  tw-whitespace-nowrap ">
-                        <th
-                          scope="col"
-                          className=" tw-text-sm tw-text-[#456DA7]  tw-font-bold tw-px-6 tw-py-4"
-                        >
-                          LEVEL
-                        </th>
-                        <th
-                          scope="col"
-                          className=" tw-text-sm tw-text-[#456DA7]  tw-font-bold tw-px-6 tw-py-4"
-                        >
-                          PERCENTAGE
-                        </th>
-
-
-
-                        <th
-                          scope="col"
-                          className=" tw-text-sm tw-text-[#456DA7]  tw-font-bold tw-px-6 tw-py-4"
-                        >
-                          Team
-                        </th>
-
-                        
-
-                        <th
-                          scope="col"
-                          className="tw-text-sm  tw-text-[#456DA7]   tw-font-bold tw-px-6 tw-py-4"
-                        >
-                          EARNING
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody className="">
-                   
-                          <>
-                            <tr className="bg-white border-t  rounded-md">
-                            <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black tw-font-poppins  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline   bg-green-200  rounded-full">
-                                 1
-                                </span>
-                              </td>
-                              <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black tw-font-poppins  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline   bg-green-200  rounded-full">
-                                 5%
-                                </span>
-                              </td>
-
-
-                              <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black tw-font-poppins  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline   bg-green-200  rounded-full">
-                                0
-                                </span>
-                              </td>
-
-                              <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline  tw-font-poppins  bg-green-200  rounded-full">
-                                0
-                                </span>
-                              </td>
-                            
-                              
-                            
-                            
-                            </tr>
-
-
-                            <tr className="bg-white border-t  rounded-md">
-                            <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black tw-font-poppins  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline   bg-green-200  rounded-full">
-                                 2
-                                </span>
-                              </td>
-                              <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black tw-font-poppins  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline   bg-green-200  rounded-full">
-                                3%
-                                </span>
-                              </td>
-
-
-                              <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black tw-font-poppins  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline   bg-green-200  rounded-full">
-                                0
-                                </span>
-                              </td>
-
-                              <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline  tw-font-poppins  bg-green-200  rounded-full">
-                                0
-                                </span>
-                              </td>
-                            
-                              
-                            
-                            
-                            </tr>
-
-                            
-                            <tr className="bg-white border-t  rounded-md">
-                            <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black tw-font-poppins  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline   bg-green-200  rounded-full">
-                                 3
-                                </span>
-                              </td>
-                              <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black tw-font-poppins  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline   bg-green-200  rounded-full">
-                                1%
-                                </span>
-                              </td>
-
-
-                              <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black tw-font-poppins  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline   bg-green-200  rounded-full">
-                                0
-                                </span>
-                              </td>
-
-                              <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
-                                <span className=" text-base text-black  py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline  tw-font-poppins  bg-green-200  rounded-full">
-                                0
-                                </span>
-                              </td>
-                            
-                              
-                            
-                            
-                            </tr>
-                          </>
-                       
-                    </tbody>
-                  </table>
-
-
-
+    {isExpanded && (
+        <div className="">
+          <table className="tw-min-w-full tw-mb-0">
+          <thead className="tw-border-t tw-border-b tw-border-[#456DA7] tw-bg-primary">
+            <tr className="tw-rounded-lg tw-whitespace-nowrap">
+              <th
+                scope="col"
+                className="tw-text-sm tw-text-[#456DA7] tw-font-bold tw-px-6 tw-py-4"
+              >
+                LEVEL
+              </th>
+              <th
+                scope="col"
+                className="tw-text-sm tw-text-[#456DA7] tw-font-bold tw-px-6 tw-py-4"
+              >
+                PERCENTAGE
+              </th>
+              <th
+                scope="col"
+                className="tw-text-sm tw-text-[#456DA7] tw-font-bold tw-px-6 tw-py-4"
+              >
+                Team
+              </th>
+              <th
+                scope="col"
+                className="tw-text-sm tw-text-[#456DA7] tw-font-bold tw-px-6 tw-py-4"
+              >
+                EARNING
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <>
+              <tr className="bg-white border-t rounded-md">
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black tw-font-poppins py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-green-200 rounded-full">
+                    1
+                  </span>
+                </td>
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black tw-font-poppins py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-green-200 rounded-full">
+                    5%
+                  </span>
+                </td>
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black tw-font-poppins py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-green-200 rounded-full">
+                    0
+                  </span>
+                </td>
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline tw-font-poppins bg-green-200 rounded-full">
+                    0
+                  </span>
+                </td>
+              </tr>
+              <tr className="bg-white border-t rounded-md">
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black tw-font-poppins py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-green-200 rounded-full">
+                    2
+                  </span>
+                </td>
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black tw-font-poppins py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-green-200 rounded-full">
+                    3%
+                  </span>
+                </td>
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black tw-font-poppins py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-green-200 rounded-full">
+                    0
+                  </span>
+                </td>
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline tw-font-poppins bg-green-200 rounded-full">
+                    0
+                  </span>
+                </td>
+              </tr>
+              <tr className="bg-white border-t rounded-md">
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black tw-font-poppins py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-green-200 rounded-full">
+                    3
+                  </span>
+                </td>
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black tw-font-poppins py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-green-200 rounded-full">
+                    1%
+                  </span>
+                </td>
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black tw-font-poppins py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline bg-green-200 rounded-full">
+                    0
+                  </span>
+                </td>
+                <td className="align-middle text-sm font-normal px-6 py-2 whitespace-nowrap text-center">
+                  <span className="text-base text-black py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline tw-font-poppins bg-green-200 rounded-full">
+                    0
+                  </span>
+                </td>
+              </tr>
+            </>
+          </tbody>
+        </table>
+        </div>
+      )}
 
                   
                   </div>
